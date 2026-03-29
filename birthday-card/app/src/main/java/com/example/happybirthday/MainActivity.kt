a69f198a98d8abe7b3e56401cdc3a823e658988b
Amanda Crotty <fullstackmandurlorian@gmail.com>
2026-03-29 13:24:03 -0400

    added birthdat text in main

--- birthday-card/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ birthday-card/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -23,13 +25,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HappyBirthdayTheme {
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                }
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ){
-                    //GreetingText(message = "Happy Birthday Tim!", from = "From Amanda")
+                    GreetingText(message = "Happy Birthday Tim!", from = "From Amanda")
                 }
             }
         }
@@ -39,18 +39,22 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingText(message: String, from: String ,modifier: Modifier = Modifier) {
     //GreetingText function displays text in the UI by calling Text() composable function
-    Text(
-        text = message,
-        fontSize = 100.sp,
-        lineHeight = 116.sp,
-        textAlign = TextAlign.Center
-    )
-    Text(
-        text = from,
-        fontSize = 36.sp
-    )
+    Column(modifier = modifier) {
+        //child element Column needs to pass the modifier parameter
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp
+        )
+    }
 }
 
+//for design view only?
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
