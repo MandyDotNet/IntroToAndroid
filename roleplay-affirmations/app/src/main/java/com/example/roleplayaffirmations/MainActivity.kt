1153b8888ccc6102f2de172f2c5470c5cf730bb4
Amanda Crotty <fullstackmandurlorian@gmail.com>
2026-04-16 11:44:00 -0400

    clean project build success

--- roleplay-affirmations/app/src/main/java/com/example/roleplayaffirmations/MainActivity.kt
+++ roleplay-affirmations/app/src/main/java/com/example/roleplayaffirmations/MainActivity.kt
@@ -3,27 +3,26 @@ package com.example.roleplayaffirmations
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.roleplayaffirmations.ui.theme.RoleplayAffirmationsTheme
 
+
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             RoleplayAffirmationsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AffirmationsApp()
                 }
             }
         }
@@ -31,17 +30,5 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RoleplayAffirmationsTheme {
-        Greeting("Android")
-    }
+fun AffirmationsApp() {
 }
