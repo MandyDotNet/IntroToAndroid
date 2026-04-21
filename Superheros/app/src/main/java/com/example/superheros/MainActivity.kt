5b516332d064ec933bcb3da3e7d01d9ee84e5abf
Amanda Crotty <fullstackmandurlorian@gmail.com>
2026-04-21 15:24:11 -0400

    resolved spelling mistake referenced

--- Superheros/app/src/main/java/com/example/superheros/MainActivity.kt
+++ Superheros/app/src/main/java/com/example/superheros/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.superheros.ui.theme.SuperherosTheme
+import com.example.superheros.ui.theme.SuperheroesTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SuperherosTheme {
+            SuperheroesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    SuperherosTheme {
+    SuperheroesTheme {
         Greeting("Android")
     }
 }
