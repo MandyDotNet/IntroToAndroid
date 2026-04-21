30d91fdfd7b0b25adb36abbb946f460eb2f590af
Amanda Crotty <fullstackmandurlorian@gmail.com>
2026-04-21 15:05:13 -0400

    updated repo for hero values

--- Superheros/app/src/main/java/com/example/superheros/model/HeroesRepository.kt
+++ Superheros/app/src/main/java/com/example/superheros/model/HeroesRepository.kt
@@ -5,11 +5,34 @@ import com.example.superheros.R
 object HeroesRepository {
     val heroes = listOf(
         Hero(
-            nameRes = R.string.hero1,
-            descriptionRes = R.string.description1,
+            nameRes = R.string.prof_x,
+            descriptionRes = R.string.profDesc,
+            imageRes = R.drawable.android_superhero1
         ),
-        Hero(),
-        Hero(),
-        Hero()
+        Hero(
+            nameRes = R.string.wolverine,
+            descriptionRes = R.string.wolfDesc,
+            imageRes = R.drawable.android_superhero2
+        ),
+        Hero(
+            nameRes = R.string.storm,
+            descriptionRes = R.string.stormDesc,
+            imageRes = R.drawable.android_superhero6
+        ),
+        Hero(
+            nameRes = R.string.gambit,
+            descriptionRes = R.string.gambitDesc,
+            imageRes = R.drawable.android_superhero3
+        ),
+        Hero(
+            nameRes = R.string.rogue,
+            descriptionRes = R.string.rogueDesc,
+            imageRes = R.drawable.android_superhero5
+        ),
+        Hero(
+            nameRes = R.string.beast,
+            descriptionRes = R.string.beastDesc,
+            imageRes = R.drawable.android_superhero4
+        )
     )
 }
