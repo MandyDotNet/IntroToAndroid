4311ab573731b2b145f9bf2e57897ea9833ffdbe
Amanda Crotty <fullstackmandurlorian@gmail.com>
2026-05-07 20:08:36 -0400

    added logic for reducing quantity of current Item being edited

--- inventory-app/app/src/main/java/com/example/inventory/ui/item/ItemDetailsScreen.kt
+++ inventory-app/app/src/main/java/com/example/inventory/ui/item/ItemDetailsScreen.kt
@@ -64,8 +64,8 @@ import androidx.compose.runtime.collectAsState
 object ItemDetailsDestination : NavigationDestination {
     override val route = "item_details"
     override val titleRes = R.string.item_detail_title
-    const val itemIdArg = "itemId"
-    val routeWithArgs = "$route/{$itemIdArg}"
+    const val ITEMIDARG = "itemId"
+    val routeWithArgs = "$route/{$ITEMIDARG}"
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
