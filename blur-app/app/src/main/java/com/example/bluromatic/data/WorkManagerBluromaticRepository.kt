32848a0b4c989dc0dd7f4113a2c1ea0a07d48efb
Amanda Crotty <fullstackmandurlorian@gmail.com>
2026-05-09 12:47:59 -0400

    update to beginUniqueWork and add TAG for image saved

--- blur-app/app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
+++ blur-app/app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
@@ -19,12 +19,15 @@ package com.example.bluromatic.data
 import android.content.Context
 import android.net.Uri
 import androidx.work.Data
+import androidx.work.ExistingWorkPolicy
 import androidx.work.OneTimeWorkRequest
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkInfo
 import androidx.work.WorkManager
+import com.example.bluromatic.IMAGE_MANIPULATION_WORK_NAME
 import com.example.bluromatic.KEY_BLUR_LEVEL
 import com.example.bluromatic.KEY_IMAGE_URI
+import com.example.bluromatic.TAG_OUTPUT
 import com.example.bluromatic.getImageUri
 import com.example.bluromatic.workers.BlurWorker
 import com.example.bluromatic.workers.CleanupWorker
@@ -47,7 +50,9 @@ class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
     override fun applyBlur(blurLevel: Int) {
 
         //add cleanup worker
-        var continuation = workManager.beginWith(
+        var continuation = workManager.beginUniqueWork(
+            IMAGE_MANIPULATION_WORK_NAME,
+            ExistingWorkPolicy.REPLACE,
             OneTimeWorkRequest.from(CleanupWorker::class.java))
 
         //blur image one time when Start button is clicked, create workRequest
@@ -58,6 +63,7 @@ class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
 
         // Add WorkRequest to save the image to the filesystem
         val save = OneTimeWorkRequestBuilder<SaveImageToFileWorker>()
+            .addTag(TAG_OUTPUT)
             .build()
         continuation = continuation.then(save)
 
