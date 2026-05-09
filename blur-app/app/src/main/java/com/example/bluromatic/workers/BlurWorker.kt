97420fe0f8a7679123859bdce77e588903b7f9ff
Amanda Crotty <fullstackmandurlorian@gmail.com>
2026-05-09 10:40:38 -0400

    added blurworker file with injected context and parameters, doWork function, and trycatch on return of the bitmap blurred

---
+++ blur-app/app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
@@ -0,0 +1,42 @@
+package com.example.bluromatic.workers
+
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.util.Log
+import com.example.bluromatic.R
+
+private const val TAG = "BlurWorker"
+
+class BlurWorker (ctx: Context, params: WorkerParameters
+    ): CoroutineWorker (ctx, params)
+    {
+        override suspend fun doWork(): Result {
+            makeStatusNotification(
+                applicationContext.resources.getString(R.string.blurring_image),
+                applicationContext
+            )
+
+            return try {
+                //populate the bitmap with the resource id
+                val picture = BitmapFactory.decodeResource(
+                    applicationContext.resources,
+                    R.drawable.android_cupcake
+                )
+                //blur the bitmap
+                val output = blurBitmap(picture,1)
+                //write bitmap to a temp file
+                val outputUri = writeBitmapToFile(applicationContext, output)
+
+                Result.success()
+            } catch (throwable: Throwable) {
+                Log.e(
+                    TAG,
+                    applicationContext.resources.getString(R.string.error_applying_blur),
+                    throwable
+                )
+                Result.failure()
+            }
+        }
+    }
