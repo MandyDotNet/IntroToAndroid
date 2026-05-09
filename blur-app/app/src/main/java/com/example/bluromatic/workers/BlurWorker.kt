e29eda077ed9f1c8dc02e0555afc68f51093278a
Amanda Crotty <fullstackmandurlorian@gmail.com>
2026-05-09 11:24:09 -0400

    pass the blur uri to BlurWorker.kt

--- blur-app/app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
+++ blur-app/app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
@@ -4,12 +4,16 @@ import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import android.content.Context
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.util.Log
 import com.example.bluromatic.DELAY_TIME_MILLIS
+import com.example.bluromatic.KEY_BLUR_LEVEL
+import com.example.bluromatic.KEY_IMAGE_URI
 import com.example.bluromatic.R
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
+import androidx.core.net.toUri
 
 private const val TAG = "BlurWorker"
 
@@ -17,6 +21,9 @@ class BlurWorker (ctx: Context, params: WorkerParameters
     ): CoroutineWorker (ctx, params)
     {
         override suspend fun doWork(): Result {
+            val resourceUri = inputData.getString(KEY_IMAGE_URI)
+            val blurLevel = inputData.getInt(KEY_BLUR_LEVEL, 1)
+
             makeStatusNotification(
                 applicationContext.resources.getString(R.string.blurring_image),
                 applicationContext
@@ -27,13 +34,22 @@ class BlurWorker (ctx: Context, params: WorkerParameters
                 delay(DELAY_TIME_MILLIS)
 
                 return@withContext try {
+                    //check that the variable is populated
+                    require(!resourceUri.isNullOrBlank()) {
+                        val errorMessage =
+                            applicationContext.resources.getString(R.string.invalid_input_uri)
+                        Log.e(TAG, errorMessage)
+                        errorMessage
+                    }
+                    //we need a content resolver obj to read the content pointed to
+                    val resolver = applicationContext.contentResolver
+
                     //populate the bitmap with the resource id
-                    val picture = BitmapFactory.decodeResource(
-                        applicationContext.resources,
-                        R.drawable.android_cupcake
+                    val picture = BitmapFactory.decodeStream( //source is passed as URI
+                        resolver.openInputStream(resourceUri.toUri()) //modern approach
                     )
                     //blur the bitmap
-                    val output = blurBitmap(picture,1)
+                    val output = blurBitmap(picture, blurLevel)
                     //write bitmap to a temp file
                     val outputUri = writeBitmapToFile(applicationContext, output)
 
